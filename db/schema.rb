62c1e15b730c64f6a682cc5c646ae07cdccd551c
5may-sytk <ec2-user@ip-172-31-18-97.eu-north-1.compute.internal>
2024-12-27 06:31:49 +0000

    再提出

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_12_26_064418) do
+ActiveRecord::Schema.define(version: 2024_12_27_063044) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2024_12_26_064418) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
